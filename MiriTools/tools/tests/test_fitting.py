c816838b16ac9463ee800bfe98b95b532405e3eb
Steven Beard <steven.beard@stfc.ac.uk>
2017-11-08 12:14:36 +0000

    Added unit tests for general spectroscopy tools.

---
+++ MiriTools/tools/tests/test_fitting.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+"""
+
+Module test_fitting - Contains the unit tests for the module called fitting.
+
+:History:
+
+09 Jan 2013: Created
+02 Sep 2013: Debugged. Suppress unwanted output.
+08 Nov 2017: Moved from LRS pipeline package to general purpose fitting
+             package.
+
+@author:  Juergen Schreiber
+
+"""
+# For consistency, import the same Python V3 features as the STScI data model.
+from __future__ import absolute_import, unicode_literals, division, print_function
+
+import unittest
+import numpy
+
+from miri.tools.fitting import gaussian, gaussPlusPoly, nonLinFit, LinFit
+
+
+class TestFitting(unittest.TestCase):
+    def setUp(self):
+        # Create an instance of Class1 and prepare it to be tested.
+        # The setUp function is automatically executed before each test.
+        self.x_data = numpy.arange(0,100)
+        self.p = (1., 100., 50., 10.)
+        self.y_data = gaussian(self.x_data, *self.p)
+        self.p_guess = (2, 110, 40, 7)
+        self.p_poly = (1., 100., 50., 10., 1., 1.)
+        self.p_poly_guess = (2, 149, 40, 7, 1.5, 1.5)
+        self.y_data_poly = gaussPlusPoly(self.x_data, *self.p_poly)
+        self.fitter = LinFit(self.x_data, self.x_data)
+
+    def tearDown(self):
+        # Clean the files and objects created.
+        # The tearDown function is automatically executed after each test.
+        del self.x_data
+        del self.y_data
+        del self.p
+        del self.p_guess
+        del self.p_poly
+        del self.p_poly_guess
+        del self.y_data_poly
+        del self.fitter
+ 
+    
+    def test_fit(self):
+        # Test the method Class1.method1
+        # See http://docs.python.org/library/unittest.html#test-cases for further
+        # information
+        #print "run gaussian fit"
+        #print gaussian
+        fit, popt, perr = nonLinFit(gaussian, self.x_data, self.y_data, p_guess=self.p_guess, verbose=False)
+        diff = max(fit) - (self.p[1]+self.p[0])
+        self.assertTrue(abs(diff) < 0.0001)
+        for i in range(len(self.p)):
+            self.assertAlmostEqual(self.p[i],popt[i])
+        
+        
+        #print "run gauss + polynomial fit"
+        fitPoly, poptPoly, perr = nonLinFit(gaussPlusPoly, self.x_data, self.y_data_poly, p_guess=self.p_poly_guess, verbose=False) 
+
+        for i in range(len(self.p)):
+            self.assertAlmostEqual(self.p_poly[i],poptPoly[i])
+
+        
+        coeff = self.fitter.fit()
+        self.assertTrue(coeff[0] == 1.)
+        
+        error = self.fitter.calcLinFitError(self.x_data)
+        self.assertTrue(numpy.mean(error) == 0.)    
+        line = self.fitter.straightLine(self.x_data)
+        self.assertTrue(numpy.all(line - self.x_data) == 0.)    
+        
+        del coeff
+        del error
+        del fitPoly
+        del popt
+        del poptPoly
+        del fit
+        del line
+        
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ MiriTools/tools/tests/test_spec_tools.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+
+"""
+
+Module test_spec_tools - Contains the unit tests for the module called spec_tools.
+
+:History:
+
+04 Feb 2014: Created
+09 Dec 2014: print statements commented out. Units tests are
+             supposed to generate minimal output.
+08 Nov 2017: Moved from LRS pipeline package to general purpose spec_tools
+             package.
+
+@author:  Juergen Schreiber
+
+"""
+# For consistency, import the same Python V3 features as the STScI data model.
+from __future__ import absolute_import, unicode_literals, division, print_function
+
+import unittest
+import numpy as np
+
+from miri.tools.spec_tools import condense, convGauss
+from miri.tools.spec_tools import lrs2D_spextract, lrs_extract_spec, \
+   lrs_extract_spec_with_fit, get_psf_fit, optimalSpecExtraction, \
+   subtractBackground, interpolWaveOnRows, interpolLin, interpolSpline
+
+from miri.datamodels.miri_measured_model import MiriMeasuredModel
+
+
+class TestCondense(unittest.TestCase):
+    def setUp(self):
+        # Create an instance of Class1 and prepare it to be tested.
+        # The setUp function is automatically executed before each test.
+        self.spec = np.arange(50, 100, 0.2)
+        self.x = np.linspace(0, 49)
+
+
+    def tearDown(self):
+        # Clean the files and objects created.
+        # The tearDown function is automatically executed after each test.
+        del self.spec
+        del self.x
+        del self.result
+
+    
+    def test_condense(self):
+        # Test the method Class1.method1
+        # See http://docs.python.org/library/unittest.html#test-cases for further
+        # information
+#         print "run condense"
+        self.result = condense(self.spec, 5)
+        self.assertTrue(len(self.spec)== len(self.result))
+        self.result = condense(self.spec, 10, method = "Mean",running = False)
+        self.assertTrue (len(self.spec)/10 == len(self.result))
+        
+        
+    def test_convGauss(self):
+#         print "run convGauss"
+        self.result = convGauss(self.x, 5, 1)
+ 
+        self.assertTrue ( np.mean(self.x) == np.mean(self.result))
+        self.assertTrue( len(self.result) == len(self.x) - 4)
+class TestLrsExtract(unittest.TestCase):
+    def setUp(self):
+        # Create an instance of Class1 and prepare it to be tested.
+        # The setUp function is automatically executed before each test.
+        a = [[10, 10, 20, 40,  20, 10, 10],
+         [10, 10, 20, 50,  20, 10, 10],
+         [10, 10, 20, 150,  20, 10, 10],
+         [10, 10, 20, 70,  50, 25, 10],
+         [10, 10, 20, 50,  20, 10, 10],
+         [10, 10, 40, 70,  30, 10, 10],
+         [10, 10, 20, 170,  20, 10, 10],
+         [10, 10, 20, 60,  20, 10, 10],
+         [10, 10, 20, 40,  20, 10, 10]]
+        err = [[0.5, 0.5, 0.2, 0.05, 0.2, 0.1, 0.5],
+         [0.6, 0.5, 0.2, 0.05, 0.2, 0.5, 0.6],
+         [0.6, 0.5, 0.2, 0.05, 0.2, 0.5, 0.6],
+         [0.5, 0.4, 0.2, 0.05, 0.2, 0.4, 0.5],
+         [0.4, 0.5, 0.2, 0.02, 0.2, 0.5, 0.5],
+         [0.1, 0.4, 0.2, 0.02, 0.2, 0.6, 0.4],
+         [0.6, 0.5, 0.2, 0.03, 0.2, 0.5, 0.6],
+         [0.6, 0.5, 0.2, 0.02, 0.2, 0.5, 0.5],
+         [0.5, 0.5, 0.2, 0.05, 0.2, 0.5, 0.4]]
+        self.dataproduct = MiriMeasuredModel(data = a, err = err)
+        self.assertTrue(hasattr(self.dataproduct,'data'))
+
+    def tearDown(self):
+        # Clean the files and objects created.
+        # The tearDown function is automatically executed after each test.
+        del self.dataproduct
+        
+    def test_lrs_extract(self):
+        # Test the method Class1.method1
+        # See http://docs.python.org/library/unittest.html#test-cases for further
+        # information
+#         print "run lrs2D_spextract"
+        prod = self.dataproduct.copy()
+        result1 = lrs2D_spextract(prod, xmin = 0, xmax = 2, ymin = 0, ymax = 2)
+
+        # All the assert* methods available in the module unittest.TestCase for
+        # python >=2.5 are listed below with a usage example
+        self.assertTrue(np.shape(result1.data)[0] ==2)
+        self.assertTrue(np.shape(result1.data)[1] ==2)
+#         print "run subtractBackground"
+        sub = subtractBackground(result1, result1)
+        self.assertTrue(sub.data[0,0] == 0.)
+#         print "run lrs_extract_spec"
+        spec = lrs_extract_spec(sub)
+        self.assertTrue(len(spec.data)== 2)
+        self.assertTrue(len(spec.err)== 2)
+#         print "run lrs_extract_spec_with_fit"
+        spec1 = lrs_extract_spec_with_fit(prod.copy(), makeplot=False,
+                                          verbose=False)
+        self.assertTrue( len(spec1) == 3)
+#         print "run optimalSpecExtract"
+        spec1 = optimalSpecExtraction(prod.copy())
+        self.assertTrue( len(spec1.data) == 9)        
+        self.assertTrue( len(spec1.err) == 9)  
+#         print "run get_psf_fit"
+        spec1 = get_psf_fit(prod.copy(), verbose=False)
+        self.assertTrue( len(spec1) == 4)
+        
+#         print "run interpolWaveOnRows"
+        wave = np.arange(5, 10, 0.5)
+        pos = np.arange(0, 5, 0.5)
+        rows, new_wave = interpolWaveOnRows(pos, wave)
+        self.assertTrue(len(rows)==5)
+        self.assertTrue(len(new_wave)==5)
+        
+        self.assertTrue(np.alltrue(np.equal(np.mod(new_wave,1),0)))
+        
+#         print "run interpolLin"
+        spec = np.arange(100, 105, 0.5)
+        new_spec = interpolLin(wave, spec, new_wave)
+        self.assertTrue(len(new_spec)==5)
+        self.assertTrue(np.alltrue(np.equal(np.mod(new_spec,1),0)))
+        
+#         print "run interpolSpline"
+        spec = np.arange(100, 105, 0.5)
+        new_spec = interpolSpline(wave, spec, new_wave)
+        self.assertTrue(len(new_spec)==5)             
+        
+        del result1, spec, spec1, sub, prod, new_spec
+
+if __name__ == '__main__':
+    unittest.main()
+
