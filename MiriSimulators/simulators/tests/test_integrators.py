cb7f2e61c8697b49f4644012cd5ea92b2218cd28
Steven Beard <steven.beard@stfc.ac.uk>
2017-12-13 10:29:53 +0000

    Test the MIRI simulator integrator at a variety of different fluxes.

--- MiriSimulators/simulators/tests/test_integrators.py
+++ MiriSimulators/simulators/tests/test_integrators.py
@@ -124,17 +124,18 @@ class TestPoissonIntegrator(unittest.TestCase):
         # Poisson noise turned off
         intnonoise = PoissonIntegrator(3, 3, simulate_poisson_noise=False,
                                        verbose=0)
-        fluxlevel = 12.345
         exptime = 100.0
-        flux = fluxlevel * np.ones((3,3), dtype=np.float32)
-        intnonoise.reset()
-        intnonoise.integrate(flux, exptime)
-        readout1 = intnonoise.readout().astype(np.int32)
-        intnonoise.integrate(flux, exptime)
-        readout2 = intnonoise.readout().astype(np.int32)
-        difference = readout2 - readout1
-        deviation = difference.mean() - (fluxlevel * exptime)
-        self.assertLess(abs(deviation), 1.0)
+        for fluxlevel in (1.234, 12.345, 123.456, 1234.56):
+            flux = fluxlevel * np.ones((3,3), dtype=np.float32)
+            intnonoise.reset()
+            intnonoise.integrate(flux, exptime)
+            readout1 = intnonoise.readout().astype(np.int32)
+            intnonoise.integrate(flux, exptime)
+            readout2 = intnonoise.readout().astype(np.int32)
+            difference = readout2 - readout1
+            # The flux difference must be less than 1 electron.
+            deviation = difference.mean() - (fluxlevel * exptime)
+            self.assertLess(abs(deviation), 1.0)
         del intnonoise
 
     def test_noise(self):
@@ -142,22 +143,23 @@ class TestPoissonIntegrator(unittest.TestCase):
         # square root of the input signal.
         # The test needs to be done using a 1000x1000 integrator
         # to eliminate the small number statistics.
-        intnoise = PoissonIntegrator(1000, 1000, verbose=0)
+        intnoise = PoissonIntegrator(1024, 1024, verbose=0)
         intnoise.set_seed(42)
-        fluxlevel = 10.0
         exptime = 10.0
-        flux = fluxlevel * np.ones((1000,1000), dtype=np.float32)
-        intnoise.reset()
-        readout1 = intnoise.readout().astype(np.int32)
-        intnoise.integrate(flux, exptime)
-        readout2 = intnoise.readout().astype(np.int32)
-        difference = readout2 - readout1
-        noise = difference.std()
-        # The following comparison can only be approximate, since the
-        # noise should approximate the square root of expected count,
-        # not the actual readings. Make sure it is within 2%.
-        deviation = difference.mean() -  (noise * noise)
-        self.assertLess(abs(deviation), difference.mean()/50.0)
+        for fluxlevel in (1.234, 12.345, 123.456, 1234.56):
+            flux = fluxlevel * np.ones((1024,1024), dtype=np.float32)
+            intnoise.reset()
+            readout1 = intnoise.readout().astype(np.int32)
+            intnoise.integrate(flux, exptime)
+            readout2 = intnoise.readout().astype(np.int32)
+            difference = readout2 - readout1
+            noise = difference.std()
+            # The following comparison can only be approximate, since the
+            # noise should approximate the square root of expected count,
+            # not the actual readings. Make sure it is within 1%.
+            deviation = difference.mean() -  (noise * noise)
+            self.assertLess(abs(deviation), difference.mean()/100.0)
+        del intnoise
 
     def test_saturation(self):
         # If a bucket size is defined, the integrator will saturate
