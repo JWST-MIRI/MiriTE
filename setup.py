2a5b0ef8ca1434c5fc51704e47b71fd763c95941
Steven Beard <steven.beard@stfc.ac.uk>
2018-08-10 14:38:22 +0100

    Added cdp_correct_band.py to setup

--- setup.py
+++ setup.py
@@ -257,6 +257,7 @@ setup(
     scripts=['miri_installation_check.py',
              'datamodels/scripts/cdp_add_history.py',
              'datamodels/scripts/cdp_add_subarray.py',
+             'datamodels/scripts/cdp_correct_band.py',
              'datamodels/scripts/cdp_get_doc.py',
              'datamodels/scripts/cdp_print.py',
              'datamodels/scripts/cdp_reduce_dark.py',
