6858601e8a08cdb28cf50470cdfb25c9f2a1f63b
steven.beard@stfc.ac.uk <steven.beard@stfc.ac.uk>
2018-10-30 17:13:01 +0000

    Extra unit tests for the flat-field models.

--- datamodels/tests/test_flatfield_model.py
+++ datamodels/tests/test_flatfield_model.py
@@ -30,6 +30,7 @@ in the datamodels.miri_flatfield_model module.
 15 Jun 2017: Do not set observation or target metadata. Neither are
              appropriate for a reference file.
 12 Jul 2017: Replaced "clobber" parameter with "overwrite".
+30 Oct 2018: Test all the new flat-field classes.
 
 @author: Steven Beard (UKATC)
 
@@ -44,7 +45,8 @@ import warnings
 import numpy as np
 
 from miri.datamodels.miri_flatfield_model import \
-    flat_reference_flags, MiriFlatfieldModel
+    flat_reference_flags, MiriFlatfieldModel, MiriSkyFlatfieldModel, \
+    MiriFringeFlatfieldModel, MiriTargetFlatfieldModel
 from miri.datamodels.tests.util import assert_products_equal
 
 
@@ -155,22 +157,402 @@ class TestMiriFlatfieldModel(unittest.TestCase):
                                        arrays=['data', 'err', 'dq'],
                                        tables='dq_def' )
                 del readback
+        
+    def test_description(self):
+        # Test that the querying and description functions work.
+        # For the test to pass these need to run without error
+        # and generate non-null strings.
+        descr = str(self.dataproduct)
+        self.assertIsNotNone(descr)
+        del descr
+        descr = repr(self.dataproduct)
+        self.assertIsNotNone(descr)
+        del descr
+        descr = self.dataproduct.stats()
+        self.assertIsNotNone(descr)
+        del descr
+        
+        # Attempt to access the SCI, ERR and DQ arrays through attributes.
+        descr = str(self.dataproduct.data)
+        self.assertIsNotNone(descr)
+        descr = str(self.dataproduct.err)
+        self.assertIsNotNone(descr)
+        del descr
+        descr = str(self.dataproduct.dq)
+        self.assertIsNotNone(descr)
+        del descr
 
-            # Check that other variations of the data product can be
-            # written to a FITS file and read back again without changing
-            # the data.
-            a1 = [[1.0,1.01,1.03,1.04], [0.95,1.05,0.97,1.08], [1.09,1.1,0.01,1.12]]
-            b1 = [[1,2,3,4],     [5,6,7,8],     [9,10,11,12]]
-            c1 = [[1,0,0,0],     [0,1,0,1],     [1,0,1,0]]
-            dqdef = flat_reference_flags
-            testproduct = MiriFlatfieldModel(data=a1, err=b1, dq=c1,
-                                              dq_def=self.dqdef,
-                                              flattype='PIXELFLAT')
-            testproduct.save(self.testfile, overwrite=True)
-            with MiriFlatfieldModel(self.testfile) as readback:
-                self.assertEqual(testproduct.meta.model_type,
+
+class TestMiriSkyFlatfieldModel(unittest.TestCase):
+    
+    # Test the MiriSkyFlatfieldModel class.
+    # Most of the relevant tests are already done in MiriMeasuredImageModel.
+    # Only the additional tests specific to MiriSkyFlatfieldModel are
+    # included here.
+    
+    def setUp(self):
+        # Create a typical flatfield product.
+        a1 = [[1.0,1.01,1.03,1.04], [0.95,1.05,0.97,1.08], [1.09,1.1,0.01,1.12]]
+        b1 = [[1,2,3,4],     [5,6,7,8],     [9,10,11,12]]
+        c1 = [[1,0,0,0],     [0,1,0,1],     [1,0,1,0]]
+        self.dqdef = flat_reference_flags
+        self.dataproduct = MiriSkyFlatfieldModel(data=a1, err=b1, dq=c1,
+                                                 dq_def=self.dqdef)
+        # Add some example metadata.
+        self.dataproduct.set_instrument_metadata(detector='MIRIFULONG',
+                                                 channel='1',
+                                                 ccc_pos='OPEN',
+                                                 deck_temperature=11.0,
+                                                 detector_temperature=6.0)
+        self.dataproduct.set_exposure_metadata(readpatt='FAST',
+                                               nints=1, ngroups=1,
+                                               frame_time=1.0,
+                                               integration_time=10.0,
+                                               group_time=10.0,
+                                               reset_time=0, frame_resets=3)
+        self.dataproduct.set_subarray_metadata('FULL')
+        self.dataproduct.set_housekeeping_metadata('UK', author='MIRI team',
+                                                   version='1.0', useafter='',
+                                                   description='Test data')
+        self.testfile = "MiriSkyFlatfieldModel_test.fits"
+        
+    def tearDown(self):
+        # Tidy up
+        del self.dataproduct
+        # Remove temporary file, if able to.
+        if os.path.isfile(self.testfile):
+            try:
+                os.remove(self.testfile)
+            except Exception as e:
+                strg = "Could not remove temporary file, " + self.testfile + \
+                    "\n   " + str(e)
+                warnings.warn(strg)
+
+    def test_referencefile(self):
+        # Check that the data product contains the standard
+        # reference file metadata.
+        type1 = self.dataproduct.meta.model_type
+        type2 = self.dataproduct.meta.reftype
+        self.assertIsNotNone(type1)
+        self.assertIsNotNone(type2)
+        pedigree = self.dataproduct.meta.pedigree
+        self.assertIsNotNone(pedigree)
+
+    def test_creation(self):
+        # It should be possible to set up an empty data product with
+        # a specified shape. All three arrays should be initialised to
+        # the same shape.
+        emptydp = MiriSkyFlatfieldModel( (4,4) )
+        self.assertIsNotNone(emptydp.data)
+        self.assertEqual(emptydp.data.shape, (4,4))
+        self.assertIsNotNone(emptydp.err)
+        self.assertEqual(emptydp.err.shape, (4,4))
+        self.assertIsNotNone(emptydp.dq)
+        self.assertEqual(emptydp.dq.shape, (4,4))
+        descr = str(emptydp)
+        self.assertIsNotNone(descr)
+        del emptydp, descr
+        
+        # A null data product can also be created and populated
+        # with data later.
+        a1 = [[1.0,1.01,1.03,1.04], [0.95,1.05,0.97,1.08], [1.09,1.1,0.01,1.12]]
+        nulldp = MiriSkyFlatfieldModel( )
+        descr1 = str(nulldp)
+        self.assertIsNotNone(descr1)
+        nulldp.data = np.asarray(a1)
+        self.assertIsNotNone(nulldp.err)
+        self.assertIsNotNone(nulldp.dq)
+        descr2 = str(nulldp)
+        self.assertIsNotNone(descr2)
+        del nulldp, descr1, descr2
+
+    def test_copy(self):
+        # Test that a copy can be made of the data product.
+        datacopy = self.dataproduct.copy()
+        self.assertIsNotNone(datacopy)
+        assert_products_equal( self, self.dataproduct, datacopy,
+                               arrays=['data', 'err', 'dq'],
+                               tables='dq_def' )
+        del datacopy
+        
+    def test_fitsio(self):
+        # Suppress metadata warnings
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+
+            # Check that the data product can be written to a FITS
+            # file and read back again without changing the data.
+            self.dataproduct.save(self.testfile, overwrite=True)
+            with MiriSkyFlatfieldModel(self.testfile) as readback:
+                self.assertEqual(self.dataproduct.meta.model_type,
                                  readback.meta.model_type)
-                assert_products_equal( self, testproduct, readback,
+                assert_products_equal( self, self.dataproduct, readback,
+                                       arrays=['data', 'err', 'dq'],
+                                       tables='dq_def' )
+                del readback
+        
+    def test_description(self):
+        # Test that the querying and description functions work.
+        # For the test to pass these need to run without error
+        # and generate non-null strings.
+        descr = str(self.dataproduct)
+        self.assertIsNotNone(descr)
+        del descr
+        descr = repr(self.dataproduct)
+        self.assertIsNotNone(descr)
+        del descr
+        descr = self.dataproduct.stats()
+        self.assertIsNotNone(descr)
+        del descr
+        
+        # Attempt to access the SCI, ERR and DQ arrays through attributes.
+        descr = str(self.dataproduct.data)
+        self.assertIsNotNone(descr)
+        descr = str(self.dataproduct.err)
+        self.assertIsNotNone(descr)
+        del descr
+        descr = str(self.dataproduct.dq)
+        self.assertIsNotNone(descr)
+        del descr
+
+
+class TestMiriFringeFlatfieldModel(unittest.TestCase):
+    
+    # Test the MiriFringeFlatfieldModel class.
+    # Most of the relevant tests are already done in MiriMeasuredImageModel.
+    # Only the additional tests specific to MiriFringeFlatfieldModel are
+    # included here.
+    
+    def setUp(self):
+        # Create a typical flatfield product.
+        a1 = [[1.0,1.01,1.03,1.04], [0.95,1.05,0.97,1.08], [1.09,1.1,0.01,1.12]]
+        b1 = [[1,2,3,4],     [5,6,7,8],     [9,10,11,12]]
+        c1 = [[1,0,0,0],     [0,1,0,1],     [1,0,1,0]]
+        self.dqdef = flat_reference_flags
+        self.dataproduct = MiriFringeFlatfieldModel(data=a1, err=b1, dq=c1,
+                                                    dq_def=self.dqdef)
+        # Add some example metadata.
+        self.dataproduct.set_instrument_metadata(detector='MIRIFUSHORT',
+                                                 channel='1',
+                                                 ccc_pos='CLOSED',
+                                                 deck_temperature=11.0,
+                                                 detector_temperature=6.0)
+        self.dataproduct.set_exposure_metadata(readpatt='FAST',
+                                               nints=1, ngroups=1,
+                                               frame_time=1.0,
+                                               integration_time=10.0,
+                                               group_time=10.0,
+                                               reset_time=0, frame_resets=3)
+        self.dataproduct.set_subarray_metadata('FULL')
+        self.dataproduct.set_housekeeping_metadata('UK', author='MIRI team',
+                                                   version='1.0', useafter='',
+                                                   description='Test data')
+        self.testfile = "MiriFringeFlatfieldModel_test.fits"
+        
+    def tearDown(self):
+        # Tidy up
+        del self.dataproduct
+        # Remove temporary file, if able to.
+        if os.path.isfile(self.testfile):
+            try:
+                os.remove(self.testfile)
+            except Exception as e:
+                strg = "Could not remove temporary file, " + self.testfile + \
+                    "\n   " + str(e)
+                warnings.warn(strg)
+
+    def test_referencefile(self):
+        # Check that the data product contains the standard
+        # reference file metadata.
+        type1 = self.dataproduct.meta.model_type
+        type2 = self.dataproduct.meta.reftype
+        self.assertIsNotNone(type1)
+        self.assertIsNotNone(type2)
+        pedigree = self.dataproduct.meta.pedigree
+        self.assertIsNotNone(pedigree)
+
+    def test_creation(self):
+        # It should be possible to set up an empty data product with
+        # a specified shape. All three arrays should be initialised to
+        # the same shape.
+        emptydp = MiriFringeFlatfieldModel( (4,4) )
+        self.assertIsNotNone(emptydp.data)
+        self.assertEqual(emptydp.data.shape, (4,4))
+        self.assertIsNotNone(emptydp.err)
+        self.assertEqual(emptydp.err.shape, (4,4))
+        self.assertIsNotNone(emptydp.dq)
+        self.assertEqual(emptydp.dq.shape, (4,4))
+        descr = str(emptydp)
+        self.assertIsNotNone(descr)
+        del emptydp, descr
+        
+        # A null data product can also be created and populated
+        # with data later.
+        a1 = [[1.0,1.01,1.03,1.04], [0.95,1.05,0.97,1.08], [1.09,1.1,0.01,1.12]]
+        nulldp = MiriFringeFlatfieldModel( )
+        descr1 = str(nulldp)
+        self.assertIsNotNone(descr1)
+        nulldp.data = np.asarray(a1)
+        self.assertIsNotNone(nulldp.err)
+        self.assertIsNotNone(nulldp.dq)
+        descr2 = str(nulldp)
+        self.assertIsNotNone(descr2)
+        del nulldp, descr1, descr2
+
+    def test_copy(self):
+        # Test that a copy can be made of the data product.
+        datacopy = self.dataproduct.copy()
+        self.assertIsNotNone(datacopy)
+        assert_products_equal( self, self.dataproduct, datacopy,
+                               arrays=['data', 'err', 'dq'],
+                               tables='dq_def' )
+        del datacopy
+        
+    def test_fitsio(self):
+        # Suppress metadata warnings
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+
+            # Check that the data product can be written to a FITS
+            # file and read back again without changing the data.
+            self.dataproduct.save(self.testfile, overwrite=True)
+            with MiriFringeFlatfieldModel(self.testfile) as readback:
+                self.assertEqual(self.dataproduct.meta.model_type,
+                                 readback.meta.model_type)
+                assert_products_equal( self, self.dataproduct, readback,
+                                       arrays=['data', 'err', 'dq'],
+                                       tables='dq_def' )
+                del readback
+        
+    def test_description(self):
+        # Test that the querying and description functions work.
+        # For the test to pass these need to run without error
+        # and generate non-null strings.
+        descr = str(self.dataproduct)
+        self.assertIsNotNone(descr)
+        del descr
+        descr = repr(self.dataproduct)
+        self.assertIsNotNone(descr)
+        del descr
+        descr = self.dataproduct.stats()
+        self.assertIsNotNone(descr)
+        del descr
+        
+        # Attempt to access the SCI, ERR and DQ arrays through attributes.
+        descr = str(self.dataproduct.data)
+        self.assertIsNotNone(descr)
+        descr = str(self.dataproduct.err)
+        self.assertIsNotNone(descr)
+        del descr
+        descr = str(self.dataproduct.dq)
+        self.assertIsNotNone(descr)
+        del descr
+
+
+class TestMiriTargetFlatfieldModel(unittest.TestCase):
+    
+    # Test the MiriTargetFlatfieldModel class.
+    # Most of the relevant tests are already done in MiriMeasuredImageModel.
+    # Only the additional tests specific to MiriTargetFlatfieldModel are
+    # included here.
+    
+    def setUp(self):
+        # Create a typical flatfield product.
+        a1 = [[1.0,1.01,1.03,1.04], [0.95,1.05,0.97,1.08], [1.09,1.1,0.01,1.12]]
+        b1 = [[1,2,3,4],     [5,6,7,8],     [9,10,11,12]]
+        c1 = [[1,0,0,0],     [0,1,0,1],     [1,0,1,0]]
+        self.dqdef = flat_reference_flags
+        self.dataproduct = MiriTargetFlatfieldModel(data=a1, err=b1, dq=c1,
+                                                    dq_def=self.dqdef)
+        # Add some example metadata.
+        self.dataproduct.set_instrument_metadata(detector='MIRIMAGE',
+                                                 channel='1',
+                                                 ccc_pos='OPEN',
+                                                 deck_temperature=11.0,
+                                                 detector_temperature=6.0)
+        self.dataproduct.set_exposure_metadata(readpatt='FAST',
+                                               nints=1, ngroups=1,
+                                               frame_time=1.0,
+                                               integration_time=10.0,
+                                               group_time=10.0,
+                                               reset_time=0, frame_resets=3)
+        self.dataproduct.set_subarray_metadata('FULL')
+        self.dataproduct.set_housekeeping_metadata('UK', author='MIRI team',
+                                                   version='1.0', useafter='',
+                                                   description='Test data')
+        self.testfile = "MiriTargetFlatfieldModel_test.fits"
+        
+    def tearDown(self):
+        # Tidy up
+        del self.dataproduct
+        # Remove temporary file, if able to.
+        if os.path.isfile(self.testfile):
+            try:
+                os.remove(self.testfile)
+            except Exception as e:
+                strg = "Could not remove temporary file, " + self.testfile + \
+                    "\n   " + str(e)
+                warnings.warn(strg)
+
+    def test_referencefile(self):
+        # Check that the data product contains the standard
+        # reference file metadata.
+        type1 = self.dataproduct.meta.model_type
+        type2 = self.dataproduct.meta.reftype
+        self.assertIsNotNone(type1)
+        self.assertIsNotNone(type2)
+        pedigree = self.dataproduct.meta.pedigree
+        self.assertIsNotNone(pedigree)
+
+    def test_creation(self):
+        # It should be possible to set up an empty data product with
+        # a specified shape. All three arrays should be initialised to
+        # the same shape.
+        emptydp = MiriTargetFlatfieldModel( (4,4) )
+        self.assertIsNotNone(emptydp.data)
+        self.assertEqual(emptydp.data.shape, (4,4))
+        self.assertIsNotNone(emptydp.err)
+        self.assertEqual(emptydp.err.shape, (4,4))
+        self.assertIsNotNone(emptydp.dq)
+        self.assertEqual(emptydp.dq.shape, (4,4))
+        descr = str(emptydp)
+        self.assertIsNotNone(descr)
+        del emptydp, descr
+        
+        # A null data product can also be created and populated
+        # with data later.
+        a1 = [[1.0,1.01,1.03,1.04], [0.95,1.05,0.97,1.08], [1.09,1.1,0.01,1.12]]
+        nulldp = MiriTargetFlatfieldModel( )
+        descr1 = str(nulldp)
+        self.assertIsNotNone(descr1)
+        nulldp.data = np.asarray(a1)
+        self.assertIsNotNone(nulldp.err)
+        self.assertIsNotNone(nulldp.dq)
+        descr2 = str(nulldp)
+        self.assertIsNotNone(descr2)
+        del nulldp, descr1, descr2
+
+    def test_copy(self):
+        # Test that a copy can be made of the data product.
+        datacopy = self.dataproduct.copy()
+        self.assertIsNotNone(datacopy)
+        assert_products_equal( self, self.dataproduct, datacopy,
+                               arrays=['data', 'err', 'dq'],
+                               tables='dq_def' )
+        del datacopy
+        
+    def test_fitsio(self):
+        # Suppress metadata warnings
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+
+            # Check that the data product can be written to a FITS
+            # file and read back again without changing the data.
+            self.dataproduct.save(self.testfile, overwrite=True)
+            with MiriTargetFlatfieldModel(self.testfile) as readback:
+                self.assertEqual(self.dataproduct.meta.model_type,
+                                 readback.meta.model_type)
+                assert_products_equal( self, self.dataproduct, readback,
                                        arrays=['data', 'err', 'dq'],
                                        tables='dq_def' )
                 del readback
