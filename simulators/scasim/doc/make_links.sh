ff6bb057e8bfb6048a8d4573ea34b94517db8b26
Steven Beard <steven.beard@stfc.ac.uk>
2020-04-16 16:30:33 +0100

    Removed references to the obsolete amplifier object

--- simulators/scasim/doc/make_links.sh
+++ simulators/scasim/doc/make_links.sh
@@ -4,7 +4,6 @@ ln -fs ../../../../doc/source/reference/find_simulator_file.rst .
 ln -fs ../../../../doc/source/reference/integrators.rst .
 #
 ln -fs ../../../../data/detector/bad_pixel.txt .
-ln -fs ../../../amplifier_properties.py .
 ln -fs ../../../cosmic_ray_properties.py .
 ln -fs ../../../detector_properties.py .
 #
@@ -16,7 +15,4 @@ ln -fs ../../../../data/detector/qe_measurementIM.txt .
 ln -fs ../../../../data/detector/qe_measurementLW.txt .
 ln -fs ../../../../data/detector/qe_measurementSW.txt .
 #
-ln -fs ../../../../data/amplifiers/read_noiseIM.txt .
-ln -fs ../../../../data/amplifiers/read_noiseLW.txt .
-ln -fs ../../../../data/amplifiers/read_noiseSW.txt .
 cd ../..
